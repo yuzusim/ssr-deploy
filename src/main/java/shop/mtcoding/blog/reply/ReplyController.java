60fff996c2a568510efc9721874ab514ecd66327
getinthere <getinthere@naver.com>
2024-03-19 15:52:31 +0900

    댓글 삭제 완료

--- src/main/java/shop/mtcoding/blog/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyController.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.reply;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.mtcoding.blog.user.User;
 
@@ -11,6 +12,13 @@ import shop.mtcoding.blog.user.User;
 public class ReplyController {
     private final ReplyService replyService;
     private final HttpSession session;
+
+    @PostMapping("/board/{boardId}/reply/{replyId}/delete")
+    public String delete(@PathVariable Integer boardId, @PathVariable Integer replyId){
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        replyService.댓글삭제(replyId, sessionUser.getId());
+        return "redirect:/board/"+boardId;
+    }
 
     @PostMapping("/reply/save")
     public String save(ReplyRequest.SaveDTO reqDTO){
--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -3,6 +3,8 @@ package shop.mtcoding.blog.reply;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.blog._core.errors.exception.Exception401;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
 import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.board.Board;
 import shop.mtcoding.blog.board.BoardJPARepository;
@@ -22,5 +24,17 @@ public class ReplyService {
         Reply reply = reqDTO.toEntity(sessionUser, board);
 
         replyJPARepository.save(reply);
+    }
+
+    @Transactional
+    public void 댓글삭제(int replyId, int sessionUserId) {
+        Reply reply = replyJPARepository.findById(replyId)
+                        .orElseThrow(() -> new Exception404("없는 댓글을 삭제할 수 없어요"));
+
+        if(reply.getUser().getId() != sessionUserId){
+            throw new Exception403("댓글을 삭제할 권한이 없어요");
+        }
+
+        replyJPARepository.deleteById(replyId);
     }
 }
